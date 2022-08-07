e043992cd3b8bc05e3ed5284adcea6495dbbf6ad
Zhao Guowei <maomaozgw@gmail.com>
2022-08-07 21:30:26 +0800

    P1220 参考了别人的做法，理解了如何从前往后动态规划做这个题目了，之前的思路的问题是把下一步可能的结果都记录下来，然后再查询，导致内存爆了，这里可以每次把前一个字母的可能性遍历一遍即可,不过性能相对于从后往前来说差了不少

--- problems/p1220/p1220.go
+++ problems/p1220/p1220.go
@@ -13,6 +13,26 @@ const (
 func countVowelPermutation(n int) int {
 	var modVal = 1000000007
 	var resultArr = []int{1, 1, 1, 1, 1}
+
+	for idx := 1; idx < n; idx++ {
+		var currentArray = make([]int, 5)
+		for charVal := 0; charVal < 5; charVal++ {
+			for _, c := range getNext(charVal) {
+				currentArray[c] = (currentArray[c] + resultArr[charVal]) % modVal
+			}
+		}
+		copy(resultArr, currentArray)
+	}
+	var result int64
+	for index := range resultArr {
+		result = (result + int64(resultArr[index])) % int64(modVal)
+	}
+	return int(result)
+}
+
+func countVowelPermutationReverse(n int) int {
+	var modVal = 1000000007
+	var resultArr = []int{1, 1, 1, 1, 1}
 	var currentArray = make([]int, 5)
 
 	for idx := 1; idx < n; idx++ {
@@ -28,4 +48,21 @@ func countVowelPermutation(n int) int {
 		result = (result + int64(resultArr[index])) % int64(modVal)
 	}
 	return int(result)
+}
+
+func getNext(c int) []int {
+	switch c {
+	case a:
+		return []int{e}
+	case e:
+		return []int{a, i}
+	case i:
+		return []int{a, e, o, u}
+	case o:
+		return []int{i, u}
+	case u:
+		return []int{a}
+	default:
+		panic("something wrong")
+	}
 }
