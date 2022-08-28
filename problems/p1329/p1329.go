66353403c8067af140bda37bfc1478ba27893d7f
Zhao Guowei <maomaozgw@gmail.com>
2022-08-28 12:16:43 +0800

    P1329 使用堆排序企图加速，结果性能下降了

--- problems/p1329/p1329.go
+++ problems/p1329/p1329.go
@@ -2,50 +2,48 @@
 
 package p1329
 
-import "sort"
+import (
+	"github.com/emirpasic/gods/trees/binaryheap"
+)
 
 func diagonalSort(mat [][]int) [][]int {
 	rl := len(mat)
 	cl := len(mat[0])
+	h := binaryheap.NewWithIntComparator()
 	for i := 0; i < cl; i++ {
-		var line []int
 		for j := 0; j < rl; j++ {
 			key := i + j
 			if key == cl {
 				break
 			}
-			line = append(line, mat[j][key])
+			h.Push(mat[j][key])
 		}
-		sort.Ints(line)
-		idx := 0
 		for j := 0; j < rl; j++ {
 			key := i + j
 			if key == cl {
 				break
 			}
-			mat[j][key] = line[idx]
-			idx++
+			val, _ := h.Pop()
+			mat[j][key] = val.(int)
 		}
 	}
 
 	for i := 1; i < rl; i++ {
-		var line []int
 		for j := 0; j < cl; j++ {
 			key := i + j
 			if key == rl {
 				break
 			}
-			line = append(line, mat[key][j])
+			h.Push(mat[key][j])
 		}
-		sort.Ints(line)
-		idx := 0
+
 		for j := 0; j < cl; j++ {
 			key := i + j
 			if key == rl {
 				break
 			}
-			mat[key][j] = line[idx]
-			idx++
+			val, _ := h.Pop()
+			mat[key][j] = val.(int)
 		}
 	}
 	return mat
