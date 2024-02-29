836841cdb3dd09ad1c76a76bdef2bce2f328eb4f
Zhao Guowei <maomaozgw@gmail.com>
2024-02-29 15:55:33 +0800

    P1609 remove debug log

--- problems/p1609/p1609.go
+++ problems/p1609/p1609.go
@@ -1,8 +1,6 @@
 package p1609
 
 import (
-	"fmt"
-
 	"github.com/maomaozgw/leetcode/structures/treenode"
 )
 
@@ -11,12 +9,10 @@ type TreeNode = treenode.BinaryTree[int]
 func isEvenOddTree(root *TreeNode) bool {
 	var nodes = []*TreeNode{root}
 	var levelOdd = true
-	treenode.Print[int](root)
 	for len(nodes) > 0 {
 		var nextNodes []*TreeNode
 		var prevNode = nodes[0]
 		if (prevNode.Val%2 == 0) == levelOdd {
-			fmt.Println(prevNode.Val, levelOdd)
 			return false
 		}
 		if prevNode.Left != nil {
@@ -27,16 +23,13 @@ func isEvenOddTree(root *TreeNode) bool {
 		}
 		for i := 1; i < len(nodes); i++ {
 			if (nodes[i].Val%2 == 0) == levelOdd {
-				fmt.Println(nodes[i].Val, levelOdd)
 				return false
 			}
 			diff := nodes[i].Val - prevNode.Val
 			if diff == 0 {
-				fmt.Printf("diff for node %d - %d is zero\n", nodes[i].Val, prevNode.Val)
 				return false
 			}
 			if (diff < 0) == levelOdd {
-				fmt.Printf("node is %d, diff is %d, levelOdd is %v\n", nodes[i].Val, diff, levelOdd)
 				return false
 			}
 			if nodes[i].Left != nil {
